0c67e5d9ca2c22dbb03242ba0d67da8fa2e18723
jonataspinto <jonatastdb_jp@hotmail.com>
2021-11-25 11:57:22 -0300

    refac: using tawild to style

--- src/components/ItemCard/index.tsx
+++ src/components/ItemCard/index.tsx
@@ -1,7 +1,7 @@
 import Modal from "react-modal";
 import { IItem, IList } from "../../types/IList";
 import { useItemCard } from "./useItemCard";
-import * as S from "./ItemCard.styled";
+import { Button } from "../Button";
 
 type ItemCardProps = {
   item: IItem;
@@ -23,21 +23,29 @@ export const ItemCard = ({ item, position }: ItemCardProps) => {
   } = useItemCard();
 
   return (
-    <S.ItemCardWrapper>
-      <S.ItemCardTitle>{item.name}</S.ItemCardTitle>
-      <S.ItemCardDescription>{item.description}</S.ItemCardDescription>
-      <S.ItemCardDescription>
-        Assinado por
-        {" "}
-        {item.subscriber?.name}
-      </S.ItemCardDescription>
-      <button
-        type="button"
-        disabled={validateActiveButton(hasSubscriber(item), signedByMe(item))}
-        onClick={() => setIsOpen(true)}
-      >
-        {getButtonLabel(hasSubscriber(item), signedByMe(item))}
-      </button>
+    <div className="card flex-row md:justify-start md:flex grid grid-cols-2 gap-3">
+      <div className="flex flex-col justify-between">
+        <img className="avatar" src={item?.photoURL} alt={`imagem ${item.name}`} />
+        <h2>{item.name}</h2>
+        <p>{item.description}</p>
+      </div>
+
+      <div className="flex flex-col justify-end gap-4">
+        <img className="avatar w-14 h-14" src={item?.subscriber?.photoURL} alt={`imagem ${item.name}`} />
+        <p>
+          Assinado por
+          {" "}
+          {item.subscriber?.name}
+        </p>
+        <Button
+          disabled={validateActiveButton(hasSubscriber(item), signedByMe(item))}
+          onClick={() => setIsOpen(true)}
+          className="secondary"
+        >
+          {getButtonLabel(hasSubscriber(item), signedByMe(item))}
+        </Button>
+      </div>
+
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
@@ -51,16 +59,16 @@ export const ItemCard = ({ item, position }: ItemCardProps) => {
           placeholder="Código de confirmação"
           name="code"
         />
-        <button
-          type="button"
+        <Button
           disabled={validateActiveButton(hasSubscriber(item), signedByMe(item))}
           onClick={() => {
             handleSubmit(list as IList, position, code?.current?.value as string);
           }}
+          className="primary"
         >
           {getButtonLabel(hasSubscriber(item), signedByMe(item))}
-        </button>
+        </Button>
       </Modal>
-    </S.ItemCardWrapper>
+    </div>
   );
 };
