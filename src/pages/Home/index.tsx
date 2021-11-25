c255c2bcbd61732700b672fe9de8bbbe8250502c
jonataspinto <jonatastdb_jp@hotmail.com>
2021-11-25 11:58:04 -0300

    feat: create styles to home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,18 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { FaWhatsapp } from "react-icons/fa";
 import { Button } from "../../components/Button";
 import { useApp } from "../../contexts/app";
 
 export const Home = () => {
   const { fetchLists, lists: data } = useApp();
+
+  const mountUrlToWhats = useCallback((number: string) => {
+    const text = "Olá, quero assinar um item. Por favor me informe o código para validação";
+    const url = `https://api.whatsapp.com/send/?phone=55${number}&text=${text}`;
+
+    return url;
+  }, []);
 
   useEffect(() => {
     fetchLists();
@@ -14,18 +22,31 @@ export const Home = () => {
 
   return (
     <section className="flex flex-col pt-4 gap-4">
-      <h1>Listas disponíveis</h1>
-      <div className="grid grid-cols-3 gap-5 ">
+      <h1 className="text-3xl font-bold text-gray-900 px-4 sm:p-0">Listas disponíveis</h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-5 ">
         {data
         && keys.map((key) => (
           <div key={data[key].id} className="card">
-            <h2>{data[key].name}</h2>
-            <img src={data[key].photoURL} alt="Imagem Familia" className="avatar" />
-            <span>{data[key].phoneNumber}</span>
+            <h2 className="text-1xl">{data[key].name}</h2>
+            <img
+              src={data[key].photoURL}
+              alt="Imagem Familia"
+              className="avatar my-1"
+            />
+            <a
+              target="_blank"
+              href={mountUrlToWhats(data[key].phoneNumber)}
+              rel="noreferrer"
+              className="flex gap-2 items-center"
+            >
+              Pegar codigo
+              <FaWhatsapp className="hover:text-green-300 transition-colors" />
+            </a>
             <Link
               to={`/list/${data[key].id}`}
             >
-              <Button className="primary">Ver lista</Button>
+              <Button className="primary mt-2">Ver lista</Button>
             </Link>
           </div>
         ))}
