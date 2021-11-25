029e6964e060bd78a76d16f24a63ae9697b566dc
jonataspinto <jonatastdb_jp@hotmail.com>
2021-11-25 11:51:03 -0300

    wip

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,35 @@
 import { useEffect } from "react";
-import { ListsSection } from "../../components/ListsSection";
+import { Link } from "react-router-dom";
+import { Button } from "../../components/Button";
 import { useApp } from "../../contexts/app";
 
 export const Home = () => {
-  const { fetchLists } = useApp();
+  const { fetchLists, lists: data } = useApp();
 
   useEffect(() => {
     fetchLists();
   }, [fetchLists]);
 
-  return <ListsSection />;
+  const keys = data ? Object.keys(data) : [];
+
+  return (
+    <section className="flex flex-col pt-4 gap-4">
+      <h1>Listas disponíveis</h1>
+      <div className="grid grid-cols-3 gap-5 ">
+        {data
+        && keys.map((key) => (
+          <div key={data[key].id} className="card">
+            <h2>{data[key].name}</h2>
+            <img src={data[key].photoURL} alt="Imagem Familia" className="avatar" />
+            <span>{data[key].phoneNumber}</span>
+            <Link
+              to={`/list/${data[key].id}`}
+            >
+              <Button className="primary">Ver lista</Button>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
