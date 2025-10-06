088b75423208b53c6f114a1d727abd390b5d20bc
jonataspinto <jonatastdb_jp@hotmail.com>
2025-10-06 17:22:09 -0300

    feat: adicionar função para copiar texto para a área de transferência

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function copyToClipboard(value?: string, callback?: () => void) {
+  if (value) {
+    navigator.clipboard.writeText(value);
+    callback?.();
+  }
+}
