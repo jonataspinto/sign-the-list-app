049f26740b6fcd49fb43b1cf235362bb499a3bef
jonataspinto <jonatastdb_jp@hotmail.com>
2025-10-09 18:34:36 -0300

    feat: add ConditionalRender component for conditional rendering of children and fallback content

---
+++ src/components/ConditionalRender.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from "react";
+
+type ConditionalRenderProps = {
+  condition?: boolean;
+  children?: ReactNode;
+  fallback?: ReactNode;
+};
+
+export function ConditionalRender({
+  condition = true,
+  fallback = null,
+  children,
+}: ConditionalRenderProps) {
+  return condition ? children : fallback;
+}
